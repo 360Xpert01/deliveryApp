7a0f04627d4533aba288ca76a6af1db1e91739e1
Huzaifa19999 <huzaifabhai10@gmail.com>
2025-02-11 14:11:54 +0500

    drawer

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ import { Provider } from 'react-redux';
 import store from './src/Redux/store'; // Import your Redux store
 import App from './App'; // Import your main App component
 import { name as appName } from './app.json';
-import DrawerNavigatior from './src/navigation/DrawerNavigator';
 
 // Wrap the App with the Redux Provider
 const Root = () => (
   <Provider store={store}>
     <App />
-    {/* <DrawerNavigatior/> */}
   </Provider>
 );
 
