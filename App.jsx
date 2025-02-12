5fdbaa76a1aca381e76487e2ab137f466f1a2f7f
AbdRehmanXplore <aabdulrehmanzul22@gmail.com>
2025-02-12 02:59:41 -0800

    loard-Board

--- App.jsx
+++ App.jsx
@@ -1,71 +1,12 @@
-// import React, { useState } from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Image, View, Text } from "react-native";
-import OrderCard from "./src/components/ordercard"; // Importing the component
-const SideBarImage = require("./assets/sidebar.png");
-
-
+import React from "react";
+// import { View, Text, StyleSheet } from "react-native";
+import LoadBoard from "./src/screens/homeScreen/home";
 
 const App = () => {
-  
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.navtime}>
-        <Image source={SideBarImage} style={styles.img} />
-        <Text style={styles.time}>00:26:23</Text>
-      </View>
-      <View style={styles.main}>
-        <ScrollView contentContainerStyle={styles.scrollView}>
-          <OrderCard codId="11,999" location="Gulshan-e-Iqbal" />
-          <OrderCard codId="9,800" location="DHA" />
-          <OrderCard codId="1,999" location="North Nazimabad" />
-          <OrderCard codId="4,295" location="Nazimabad" />
-          <OrderCard codId="1,800" location="PECHS" />
-          <OrderCard codId="5,208" location="Gulshan Iqbal" />
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+    <LoadBoard/>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1,
-    backgroundColor: "#FAFAFA",
-    
-  },
-  scrollView: {
-    paddingBottom: 100,
-  },
-  img: {
-    height: 58,
-    width: 58,
-    paddingTop: 51,
-    paddingBottom: 21,
-  },
-  main: {
-    backgroundColor: "#F9F9F9",
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    paddingTop: 19,
-  },
-  time: {
-    // width: 177,
-    height: 45,
-    fontSize: 32,
-    fontWeight: "bold",
-    color: "white",
-    textAlign: "center",
-    textAlignVertical: "center",
-    color: "#00AA2F",
-  },
-  navtime:{
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-
-  }
-});
 
 export default App;
--- src/screens/homeScreen/home.jsx
+++ src/screens/homeScreen/home.jsx
@@ -1,46 +1,71 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+// import React, { useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, Image, View, Text } from "react-native";
+import OrderCard from "../../components/ordercard";
+const SideBarImage = require("../../../assets/sidebar.png")
 
-const HomeScreen = () => {
-  const navigation = useNavigation();
+
+const LoadBoard = () => {
+  
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.navtime}>
+        <Image source={SideBarImage} style={styles.img} />
+        <Text style={styles.time}>00:26:23</Text>
+      </View>
+      <View style={styles.main}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          <OrderCard codId="11,999" location="Gulshan-e-Iqbal" />
+          <OrderCard codId="9,800" location="DHA" />
+          <OrderCard codId="1,999" location="North Nazimabad" />
+          <OrderCard codId="4,295" location="Nazimabad" />
+          <OrderCard codId="1,800" location="PECHS" />
+          <OrderCard codId="5,208" location="Gulshan Iqbal" />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
-HomeScreen.options = ({ navigation }) => ({
-  title: 'Home',
-  headerRight: () => (
-    <Icon
-      name="bell"
-      size={wp('6%')} 
-      color="black"
-      onPress={() => navigation.navigate('Notifications')}
-      style={styles.icon}
-    />
-  ),
-});
-
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: wp('5%'), 
+    backgroundColor: "#FAFAFA",
+    
   },
-  text: {
-    fontSize: wp('6%'),
-    textAlign: 'center',
+  scrollView: {
+    paddingBottom: 100,
   },
-  icon: {
-    marginRight: wp('2.5%'),
+  img: {
+    height: 58,
+    width: 58,
+    paddingTop: 51,
+    paddingBottom: 21,
   },
+  main: {
+    backgroundColor: "#F9F9F9",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingTop: 19,
+  },
+  time: {
+    // width: 177,
+    height: 45,
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: "#00AA2F",
+    paddingRight:20,
+  },
+  navtime:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+  }
 });
 
-export default HomeScreen;
+export default LoadBoard;
